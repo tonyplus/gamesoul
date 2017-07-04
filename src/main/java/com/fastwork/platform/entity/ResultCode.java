f9f92c7a27d94bfa1b92a280cefc0ff7b41673e7
xufeng <64554173@qq.com>
2017-07-04 22:23:44 +0800

    游戏信息接口

---
+++ src/main/java/com/fastwork/platform/entity/ResultCode.java
@@ -0,0 +1,19 @@
+package com.fastwork.platform.entity;
+
+public class ResultCode {
+
+	/**
+	 * 返回成功
+	 */
+	public static String  SUCCESS="0";
+	
+	/**
+	 * 系统繁忙，请稍候再试
+	 */
+	public static String  ERROR="-1";
+	
+	/**
+	 * 文件不存在
+	 */
+	public static String  NOFILE="1001";
+}
